e8b8d9d7581b2b9d40fbaa26ce334f6bd9090e5a
rE <haxrelt@gmail.com>
2024-03-03 19:01:02 +0300

    custom logger

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,6 +6,7 @@ import (
 	echomw "github.com/labstack/echo/v4/middleware"
 	"github.com/sladkoezhkovo/gateway/internal/config"
 	"github.com/sladkoezhkovo/gateway/internal/handler"
+	"github.com/sladkoezhkovo/gateway/pkg/colors"
 	"net/http"
 )
 
@@ -15,6 +16,13 @@ type server struct {
 
 	auth *handler.AuthHandler
 }
+
+const (
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
+)
 
 func New(cfg *config.HttpConfig, auth handler.AuthService) *server {
 	return &server{
@@ -26,10 +34,48 @@ func New(cfg *config.HttpConfig, auth handler.AuthService) *server {
 
 func (s *server) setup() {
 	s.e.Use(echomw.CORS())
-	s.e.Use(echomw.LoggerWithConfig(echomw.LoggerConfig{
-		Skipper:          echomw.DefaultSkipper,
-		Format:           "method=${method}, uri=${uri}, status=${status}\n",
-		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+	//s.e.Use(echomw.LoggerWithConfig(echomw.LoggerConfig{
+	//	Skipper:          echomw.DefaultSkipper,
+	//	Format:           "${method}\t${uri}\t| ${status}\ttime=${time_rfc3339}\n",
+	//	CustomTimeFormat: "2006-01-02 15:04:05.00000",
+	//}))
+
+	// SWITCH LOGGING LOGIC by config.env
+	s.e.Use(echomw.RequestLoggerWithConfig(echomw.RequestLoggerConfig{
+		LogStatus: true,
+		LogMethod: true,
+		LogURI:    true,
+		LogValuesFunc: func(c echo.Context, v echomw.RequestLoggerValues) error {
+
+			methodColor := colors.White
+			statusColor := colors.Green
+
+			switch v.Method {
+			case GET:
+				methodColor = colors.Green
+			case POST:
+				methodColor = colors.Yellow
+			case DELETE:
+				methodColor = colors.Red
+			case PUT:
+				methodColor = colors.Cyan
+			}
+
+			if v.Status >= 500 {
+				statusColor = colors.Red
+			} else if v.Status >= 400 {
+				statusColor = colors.Yellow
+			} else if v.Status >= 300 {
+				statusColor = colors.Cyan
+			}
+
+			fmt.Printf("%s%s%s\t", methodColor, v.Method, colors.Reset)
+			fmt.Printf("%s\t", v.URI)
+			fmt.Printf("| %s%d%s\t", statusColor, v.Status, colors.Reset)
+			fmt.Printf("%s\n", v.StartTime)
+
+			return nil
+		},
 	}))
 
 	s.e.GET("/ping", func(c echo.Context) error {
